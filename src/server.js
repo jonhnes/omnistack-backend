1ffcd0716433d2f52a436b50b482034d9dd67737
Jonhnes Lopes <jonhnes@gmail.com>
2019-10-05 19:49:54 -0300

    Configuring database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const routes = require('./routes')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const routes = require('./routes');
 
 const server = express();
+
+dotenv.config();
+mongoose.connect(process.env.DB_CONNECTION_URL, {
+  useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);
