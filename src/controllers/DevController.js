288d74985c4d8d74b3f3978d0edef424531b4d2f
Jonhnes Lopes <jonhnes@gmail.com>
2019-10-05 20:04:06 -0300

    [Controller] Saving user data from github

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,10 +1,20 @@
-const axios = require('axios')
+const axios = require('axios');
+const Dev = require('../models/Dev');
 module.exports = {
   async store(req, res) {
-    const { username } = req.body;
+    const { username: user } = req.body;
 
-    const response = await axios.get(`https://api.github.com/users/${username}`)
+    const response = await axios.get(`https://api.github.com/users/${user}`);
 
-    return res.json(response.data)
+    const { name, bio, avatar_url: avatar } = response.data
+
+    const dev = await Dev.create({
+      name,
+      user,
+      bio,
+      avatar
+    })
+
+    return res.json(dev)
   }
 }
