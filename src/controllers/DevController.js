bc4b18e61566e52d50a5b43da684351258fd4916
Jonhnes Lopes <jonhnes@gmail.com>
2019-10-05 20:16:06 -0300

    [Controller] Verifying if user exists

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -4,6 +4,12 @@ module.exports = {
   async store(req, res) {
     const { username: user } = req.body;
 
+    const userExists = await Dev.findOne({ user })
+
+    if  (userExists) {
+      return res.json(userExists);
+    }
+
     const response = await axios.get(`https://api.github.com/users/${user}`);
 
     const { name, bio, avatar_url: avatar } = response.data
